7bbb2ee5505cdfbbeb55f931d41ff11cafdd0643
Alessandro Cappellozza <alessandro.cappellozza@gmail.com>
2020-03-17 09:57:41 +0100

    Feature card overlap

--- source/_nav/menu-responsive.blade.php
+++ source/_nav/menu-responsive.blade.php
@@ -1,5 +1,5 @@
-<nav id="js-nav-menu" class="nav-menu hidden lg:hidden">
-    <ul class="my-0">
+<nav id="js-nav-menu" class="nav-menu hidden lg:hidden z-10 ml-2 mr-2">
+    <ul class="my-0 list-none">
         <li class="pl-4">
             <a
                 title="{{ $page->siteName }} Blog"
